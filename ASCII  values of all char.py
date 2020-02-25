2c3c88f27dc3ce970a084ece10c0be67acff4be1
aaru1199 <61352793+aaru1199@users.noreply.github.com>
2020-02-25 21:31:11 +0530

    Add files via upload

---
+++ ASCII  values of all char.py
@@ -0,0 +1,4 @@
+str1 = input("Please Enter your Own String : ")
+ 
+for i in range(len(str1)):
+    print("The ASCII Value of Character %c = %d" %(str1[i], ord(str1[i])))
---
+++ odd numbers of 1 to 100.py
@@ -0,0 +1,6 @@
+
+num = int(input(" Please Enter any Maximum Value : "))
+
+for i in range(1, num + 1):
+    if(i % 2 != 0):
+        print("{0}".format(i))
