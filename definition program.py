99245366b9ac11716cc4e63a3cce627545340a8d
aaru1199 <61352793+aaru1199@users.noreply.github.com>
2020-03-07 20:25:18 +0530

    Add files via upload

---
+++ definition program.py
@@ -0,0 +1,5 @@
+def solveme(a,b):
+    num1=int(input("number 1:")
+    b=int(input("number 2:")
+res=solveme(num1,num2)
+print (res)              
---
+++ dictionaries.py
@@ -0,0 +1,23 @@
+a={1:'apple',2:'grape',3:'orange',4:'banana',5:'mango'}
+
+#for i,j in a.items():
+  #  print(i,j)
+#for i in a.keys():
+ #   print(i)
+#for j in a.values():
+    print(j)
+choice=int(input("enter the number:"i))
+
+for i,j in a.items():    
+    if (choice==i):
+            print ("the value is:",j)
+#print(a[c]//**instead of for loop we can use this**//
+      
+#b=a.pop(c)//**instead of for loop we can use this**//
+#print (b)
+            
+
+
+      
+
+    
